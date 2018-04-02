e9f0de3f7207d159d86ce9ea4a13a711ce68d29b
Andre MakaroFF <makarofff@ukr.net>
2018-04-03 01:16:22 +0300

    27.03 geotesting 95%

--- src/ButtonSwitcher/ButtonSwitcher.js
+++ src/ButtonSwitcher/ButtonSwitcher.js
@@ -4,20 +4,65 @@ export class ButtonSwitcher extends React.Component {
   state = {
     active: false,
     user: '',
-    users: '',
+    users: [],
+    nearestUser: {
+      userName: '',
+      distance: '',
+    },
     location: {
       latitude: '',
       longitude: '',
     }
   };
+  constructor(props){
+    super(props);
+    this.getGeo();
+  }
 
   handlerBtn = () => {
     this.setState(prevState => ({ active: !prevState.active }));
   };
   getUsers = () => {
-
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(data => data.json())
+      .then(users => this.setState({
+        users,
+      }))
   };
   handlerBtnNearest = () => {
+    let distance = Infinity;
+    let userName = '';
+    let minLen = this.state.users.map(user => {
+      const currentName = user.name;
+      const lat1 = user.address.geo.lat;
+      const lon1 = user.address.geo.lng;
+      const lat2 = this.state.location.latitude;
+      const lon2 = this.state.location.longitude;
+
+      const calcDistance = (lat1, lon1, lat2, lon2) => {
+        const p = Math.PI / 180;
+        const c = Math.cos;
+        const a = 0.5 - c((lat2 - lat1) * p) / 2 +
+          c(lat1 * p) * c(lat2 * p) *
+          (1 - c((lon2 - lon1) * p)) / 2;
+
+        const result = 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+        if (distance > result) {
+          distance = result;
+          userName = currentName;
+        }
+        console.log(result, currentName);
+      };
+      console.log(calcDistance(lat1, lon1, lat2, lon2));
+    });
+    this.setState({
+      nearestUser: {
+        userName,
+        distance
+      }
+    });
+  };
+  getGeo = () => {
     navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({
@@ -25,15 +70,6 @@ export class ButtonSwitcher extends React.Component {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
           }
-        });
-        fetch('https://jsonplaceholder.typicode.com/users')
-          .then(data => data.json())
-          .then(users => this.setState({
-            users: users.sort((prev, next) => prev.name > next.name)
-          }));
-
-        this.setState({
-          user: 'fgbfgb'
         });
       },
       () => {
@@ -68,6 +104,9 @@ export class ButtonSwitcher extends React.Component {
         >
           {this.state.active ? 'Hide' : 'Show'}
         </button>
+
+
+
         <button
           className={`buttonSwitcher__btn ${this.state.active && 'active'}`}
           onClick={this.handlerBtnGeo}
@@ -76,16 +115,29 @@ export class ButtonSwitcher extends React.Component {
           {this.state.location.latitude}
           {this.state.location.longitude}
         </button>
+        <br/>
+
+        <button
+          className={`buttonSwitcher__btn ${this.state.active && 'active'}`}
+          onClick={this.getUsers}
+        >
+          getUsers
+        </button>
         <button
           className={`buttonSwitcher__btn ${this.state.active && 'active'}`}
           onClick={this.handlerBtnNearest}
         >
-          nearest
-          {this.state.user}
+          handlerBtnNearest
         </button>
+
         <b>
           {this.state.user}
         </b>
+        <b>
+          {this.state.nearestUser.userName}<br/>
+          {this.state.nearestUser.distance}
+        </b>
+
         <p>
           {this.state.active && 'Hidden text here!'}
         </p>
